6a3abb5d604f4c04be86c0303e590e5bbf0b2d21
Artur Bondar <bondarth@gmail.com>
2021-03-21 10:39:33 +0200

    add swagger support

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,33 @@
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { SignUpDto } from './dto/signUp.dto';
 import { AuthService } from './auth.service';
 import { Public } from 'src/decorators/noAuth.decorator';
 
+@ApiTags('auth')
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
   @Post('signIn')
+  @ApiCreatedResponse({ description: 'User successfully authorized.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   signIn(@Body() user: LoginDto) {
     return this.authService.signIn(user);
   }
 
   @Public()
   @Post('signUp')
+  @ApiCreatedResponse({ description: 'User has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Bad request body.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   async signUp(@Body() newUser: SignUpDto) {
     return this.authService.signUp(newUser);
   }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,32 @@
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { UpdateDto } from './dto/update.dto';
 import { Body, Controller, Get, Patch, Request } from '@nestjs/common';
 import { UserService } from './user.service';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User data has been provided.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   getUserData(@Request() req) {
     return this.userService.get(req.user.id);
   }
 
   @Patch('update')
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'User data has been changed.' })
+  @ApiBadRequestResponse({ description: 'Bad request body.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
   updateUserData(@Request() req, @Body() updateUser: UpdateDto) {
     return this.userService.update(req.user.id, updateUser);
   }
